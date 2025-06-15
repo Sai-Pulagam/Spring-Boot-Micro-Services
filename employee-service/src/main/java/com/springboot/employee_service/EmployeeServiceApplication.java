e432cdfe89ef60f4c62441982e31de3390d1f1d1
sai-pulagam <sai.pulagam@zohomail.com.au>
2025-06-15 22:33:56 +0530

    add webclient in employee-service

--- employee-service/src/main/java/com/springboot/employee_service/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/springboot/employee_service/EmployeeServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
@@ -12,6 +13,10 @@ public class EmployeeServiceApplication {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
 
+	@Bean
+	public WebClient webClient(){
+		return WebClient.builder().build();
+	}
 
 	@Bean
 	public RestTemplate restTemplate(){
--- employee-service/src/main/java/com/springboot/employee_service/service/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/springboot/employee_service/service/EmployeeServiceImpl.java
@@ -9,6 +9,7 @@ import com.springboot.employee_service.respository.EmployeeRespository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
@@ -22,12 +23,16 @@ public class EmployeeServiceImpl implements  EmployeeService{
 
     private RestTemplate restTemplate;
 
+    private WebClient webClient;
+
     public  EmployeeServiceImpl(EmployeeRespository employeeRespository,
                                 ObjectMapper objectMapper,
-                                RestTemplate restTemplate){
+                                RestTemplate restTemplate,
+                                WebClient webClient){
         this.employeeRespository = employeeRespository;
         this.objectMapper = objectMapper;
         this.restTemplate = restTemplate;
+        this.webClient = webClient;
     }
 
     @Override
@@ -42,9 +47,16 @@ public class EmployeeServiceImpl implements  EmployeeService{
         Employee employee = result.get();
         EmployeeDTO employeeDTO = objectMapper.convertValue(employee,EmployeeDTO.class);
 
-        ResponseEntity<DepartmentDTO> response = restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode()
-        , DepartmentDTO.class);
-        DepartmentDTO departmentDTO = response.getBody();
+//        ResponseEntity<DepartmentDTO> response = restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode()
+//        , DepartmentDTO.class);
+
+        DepartmentDTO departmentDTO = webClient.get()
+                .uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDTO.class)
+                .block();
+
+//        DepartmentDTO departmentDTO = response.getBody();
 
         APIResponseDTO apiResponseDTO = new APIResponseDTO();
         apiResponseDTO.setEmployeeDTO(employeeDTO);
