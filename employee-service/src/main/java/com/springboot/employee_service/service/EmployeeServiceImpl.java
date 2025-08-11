57835bad23fb72160295823d430eb6113ac34fdd
karthik <17pa1a0277@vishnu.edu.in>
2025-08-12 00:49:26 +1000

    added resilience4j

--- employee-service/src/main/java/com/springboot/employee_service/service/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/springboot/employee_service/service/EmployeeServiceImpl.java
@@ -6,6 +6,12 @@ import com.springboot.employee_service.dto.DepartmentDTO;
 import com.springboot.employee_service.dto.EmployeeDTO;
 import com.springboot.employee_service.entity.Employee;
 import com.springboot.employee_service.respository.EmployeeRespository;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -26,6 +32,8 @@ public class EmployeeServiceImpl implements  EmployeeService{
     private WebClient webClient;
 
     private APIClient apiClient;
+    
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     public  EmployeeServiceImpl(EmployeeRespository employeeRespository,
                                 ObjectMapper objectMapper,
@@ -46,7 +54,11 @@ public class EmployeeServiceImpl implements  EmployeeService{
     }
 
     @Override
+//    @CircuitBreaker(name = "${spring.application.name}",fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     public APIResponseDTO getEmployeeByID(Long employeeID) {
+    	
+    	logger.info("<---------inside the getEmployeeByID Method-------->");
         Optional<Employee> result = employeeRespository.findById(employeeID);
         Employee employee = null;
         if(result.isPresent())
@@ -70,6 +82,30 @@ public class EmployeeServiceImpl implements  EmployeeService{
         apiResponseDTO.setEmployeeDTO(employeeDTO);
         apiResponseDTO.setDepartmentDTO(departmentDTO);
         return apiResponseDTO;
+    }
+    
+    public APIResponseDTO getDefaultDepartment(Long employeeID, Exception exception) {
+    	
+    	logger.info("<-------- inside the getDefaultDepartmentMethod() ----------->");
+    	
+    	
+    	Optional<Employee> result = employeeRespository.findById(employeeID);
+        Employee employee = null;
+        if(result.isPresent())
+            employee = result.get();
+        EmployeeDTO employeeDTO = objectMapper.convertValue(employee,EmployeeDTO.class);
+
+        DepartmentDTO departmentDTO = new DepartmentDTO();
+        departmentDTO.setDepartmentCode("RD001");
+        departmentDTO.setDepartmentName("R&D Department");
+        departmentDTO.setDepartmentDescription("Research and Development Department");
+        
+
+        APIResponseDTO apiResponseDTO = new APIResponseDTO();
+        apiResponseDTO.setEmployeeDTO(employeeDTO);
+        apiResponseDTO.setDepartmentDTO(departmentDTO);
+        return apiResponseDTO;
+    	
     }
 
 }
