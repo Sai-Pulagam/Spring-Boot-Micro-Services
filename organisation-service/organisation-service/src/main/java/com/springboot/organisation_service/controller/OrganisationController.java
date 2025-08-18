f40b3bf7246da12aaf6196531703ccf11d5edb3e
karthik <17pa1a0277@vishnu.edu.in>
2025-08-18 15:56:51 +1000

    add Swagger UI configuration for Organisation API documentation

--- organisation-service/organisation-service/src/main/java/com/springboot/organisation_service/controller/OrganisationController.java
+++ organisation-service/organisation-service/src/main/java/com/springboot/organisation_service/controller/OrganisationController.java
@@ -13,6 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.organisation_service.dto.OrganisationDTO;
 import com.springboot.organisation_service.service.OrganisationService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(
+		
+		name = "Organisation Service - Organisation Controller",
+		description = "Organisation Controller exposes REST APIs for organisation service")
+
 @RestController
 @RequestMapping("/api/organisations")
 public class OrganisationController {
@@ -22,12 +31,26 @@ public class OrganisationController {
 	public OrganisationController(OrganisationService organisationService) {
 		this.organisationService = organisationService;
 	}
-	
+	@Operation(
+			summary = "Save Organisation REST API",
+			description = "SaveOrganisation REST API is used to save the organisation object in the database")
+	@ApiResponse(
+			responseCode = "201",
+			description = "Http Status 201 Created"
+			)
 	@PostMapping
 	public ResponseEntity<OrganisationDTO> saveOrganisation(@RequestBody OrganisationDTO organisationDTO){
 		 OrganisationDTO savedOrganisation = organisationService.saveOrganisation(organisationDTO);
 		 return new ResponseEntity<>(savedOrganisation,HttpStatus.CREATED);
 	}
+	
+	@Operation(
+			summary = "GEt Organisation REST API",
+			description = "GetOrganisation REST API is used to get the organisation object from the database")
+	@ApiResponse(
+			responseCode = "200",
+			description = "Http Status 200 OK"
+			)
 	
 	@GetMapping("/{organisationCode}")
 	public ResponseEntity<OrganisationDTO> getOrganisationByOrganisationCode(@PathVariable String organisationCode){
--- organisation-service/organisation-service/src/main/java/com/springboot/organisation_service/dto/OrganisationDTO.java
+++ organisation-service/organisation-service/src/main/java/com/springboot/organisation_service/dto/OrganisationDTO.java
@@ -2,18 +2,29 @@ package com.springboot.organisation_service.dto;
 
 import java.time.LocalDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description =  "Organisation Model Information")
 public class OrganisationDTO {
-	
+
 	private Long id;
+
+	@Schema(description = "Organisation Name")
 	private String organisationName;
+	@Schema(description = "Organisation Description")
 	private String organisationDescription;
+
+	@Schema(description = "Organisation Code")
 	private String organisationCode;
+
+	@Schema(description = "Organisation Created Date")
 	private LocalDateTime createdDate;
-	
+
 	public OrganisationDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public OrganisationDTO(Long id, String organisationName, String organisationDescription, String organisationCode,
 			LocalDateTime createdDate) {
 		super();
@@ -23,42 +34,52 @@ public class OrganisationDTO {
 		this.organisationCode = organisationCode;
 		this.createdDate = createdDate;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getOrganisationName() {
 		return organisationName;
 	}
+
 	public void setOrganisationName(String organisationName) {
 		this.organisationName = organisationName;
 	}
+
 	public String getOrganisationDescription() {
 		return organisationDescription;
 	}
+
 	public void setOrganisationDescription(String organisationDescription) {
 		this.organisationDescription = organisationDescription;
 	}
+
 	public String getOrganisationCode() {
 		return organisationCode;
 	}
+
 	public void setOrganisationCode(String organisationCode) {
 		this.organisationCode = organisationCode;
 	}
+
 	public LocalDateTime getCreatedDate() {
 		return createdDate;
 	}
+
 	public void setCreatedDate(LocalDateTime createdDate) {
 		this.createdDate = createdDate;
 	}
+
 	@Override
 	public String toString() {
 		return "OrganisationDto [id=" + id + ", organisationName=" + organisationName + ", organisationDescription="
 				+ organisationDescription + ", organisationCode=" + organisationCode + ", createdDate=" + createdDate
 				+ "]";
 	}
-	
-	
+
 }
